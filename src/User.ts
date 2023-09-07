150b58e3ea4b939ed8c2861a18154fb7e45911d1
Lebatt <lebatt.eleyouta98@gmail.com>
2023-09-07 20:53:55 +0000

    user crud without jwt

--- src/User.ts
+++ src/User.ts
@@ -5,6 +5,31 @@ const router = express.Router();
 
 const prisma = new PrismaClient();
 
+router.get("/", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.send(users);
+  } catch (error) {
+    console.error("Error listing users:", error);
+    res.status(500).json({ error: "Users listed failed" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await prisma.user.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    });
+    res.send(user);
+  } catch (error) {
+    console.error("Error search users:", error);
+    res.status(500).json({ error: "Users searshing failed" });
+  }
+});
+
 router.post("/create", async (req, res) => {
   try {
     const { nom, email, password } = req.body;
@@ -19,6 +44,42 @@ router.post("/create", async (req, res) => {
   } catch (error) {
     console.error("Error creating user:", error);
     res.status(500).json({ error: "User creation failed" });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { nom, email, password } = req.body;
+    const upUser = await prisma.user.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: {
+        nom,
+        email,
+        password,
+      },
+    });
+    res.json(upUser);
+  } catch (error) {
+    console.error("Error editing user:", error);
+    res.status(500).json({ error: "User editing failed" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const delUser = await prisma.user.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
+    res.send("user deleted succesfully !");
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    res.status(500).json({ error: "User delete failed" });
   }
 });
 
